faced7d2167a367d34d066cdad20a2987e1fa0e3
ElooyGM <93944488+ElooyGM@users.noreply.github.com>
2023-10-24 12:59:48 +0200

    13:00 Eloy

--- functions/functions-structure.php
+++ functions/functions-structure.php
@@ -30,13 +30,13 @@
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pedido</a>
+                    <a class="nav-link" href="Pedido.php">Pedido</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Carta</a>
+                    <a class="nav-link" href="Carta.php">Carta</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="Login.php">Login</a>
                 </li>
                 </ul>
             </div>
